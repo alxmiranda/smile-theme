a382b29e4b88fdee0d4135368ded0b11763a0e47
Alex <amdesignercontato@gmail.com>
2015-06-26 16:39:46 -0300

    Marcação da estrutura home, bem como a inclusão do topo e rodape

--- index.php
+++ index.php
@@ -1,25 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>Teste grid</title>
-	<link rel="stylesheet" href="assets/css/style.min.css" type="text/css">
-</head>
-<body>
-	<div class="container">
-	<div class="border col-1">ìtem 1</div>
-	<div class="border col-2">ìtem 2</div>
-	<div class="border col-3">ìtem 3</div>
-	<div class="border col-4">ìtem 4</div>
-	<div class="border col-5">ìtem 5</div>
-	<div class="border col-6">ìtem 6</div>
-	<div class="border col-7">ìtem 7</div>
-	<div class="border col-8">ìtem 8</div>
-	<div class="border col-9">ìtem 9</div>
-	<div class="border col-10">ìtem 10</div>
-	<div class="border col-11">ìtem 11</div>
-	<div class="border col-12">ìtem 12</div>
-	</div>
-</body>
-</html>
+<?php include "header.php"; ?>
+
+
+<main>
+	<section class="banner banner-home"></section>
+	<section class="section-full cta"></section>
+	<section class="section-full produtos"></section>
+	<section class="section-full sobre-nos"></section>
+	<section class="section-full fornecedores"></section>
+	<section class="section-full blog"></section>
+</main>
+
+
+<?php include "footer.php"; ?>
