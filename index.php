b2a9b1d031abb798701160d504bd9f723b445209
Alex <amdesignercontato@gmail.com>
2015-06-26 15:04:21 -0300

    Primeiro teste com grid ok. Aqui foi Criado as classes para as colunas em desktop como teste. Neste commit também foi atualizada a pasta de documentação, incluindo a descrição do layout segundo o designer.

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Smile theme</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>Teste grid</title>
+	<link rel="stylesheet" href="assets/css/style.min.css" type="text/css">
 </head>
 <body>
-	
+	<div class="container">
+	<div class="border col-1">ìtem 1</div>
+	<div class="border col-2">ìtem 2</div>
+	<div class="border col-3">ìtem 3</div>
+	<div class="border col-4">ìtem 4</div>
+	<div class="border col-5">ìtem 5</div>
+	<div class="border col-6">ìtem 6</div>
+	<div class="border col-7">ìtem 7</div>
+	<div class="border col-8">ìtem 8</div>
+	<div class="border col-9">ìtem 9</div>
+	<div class="border col-10">ìtem 10</div>
+	<div class="border col-11">ìtem 11</div>
+	<div class="border col-12">ìtem 12</div>
+	</div>
 </body>
 </html>
