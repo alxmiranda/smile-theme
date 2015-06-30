e7a53891826b1d9ef8d3ff1dde90442c2685985a
Alex <amdesignercontato@gmail.com>
2015-06-30 10:55:49 -0300

    Removendo arquivos do exemplo de compotente form

--- dev/assets/sass/componentes/forms/form-2.php
+++
@@ -1,7 +0,0 @@
-<h2>Faça um orçamento</h2>
-<form action="" class="contato">
-	<input type="text" placeholder="Nome">
-	<input type="text" placeholder="Assunto">
-	<input type="textarea" placeholder="Mensagem">
-	<input type="submit" value="Enviar" />
-</form>
--- dev/assets/sass/componentes/forms/form-3.php
+++
@@ -1,4 +0,0 @@
-<h2>Duvidas</h2>
-<input type="text" placeholder="Nome">
-<input type="textarea" placeholder="Duvida">
-<input type="submit" placeholder="Enviar">
