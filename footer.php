d2b4e71eee8725938d85e3ca20ba06fef9b9bcc5
Alex <amdesignercontato@gmail.com>
2015-07-20 14:14:02 -0300

    Delentando arquivos de exemplo

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="rodape">
-	<section class="rodape__cta-contato"></section>
-	<section class="rodape__informacoes"></section>
-	<section class="rodape__copy-e-creditos"></section>
-</footer>
